49c8842f4fa47cedca32005dc1c85fd547bca74e
Speros Kokenes <speroskokenes@gmail.com>
2017-09-28 13:12:38 -0400

    Updated retryLayout object to use handle call syntax

--- src/retryLayouts.js
+++ src/retryLayouts.js
@@ -1,6 +1,6 @@
 import { invalidated } from "rxq/operators/handle";
 import { getLayout } from "rxq/operators/genericobject";
-import { map, windowCount, catchError, switchAll, retryWhen, delay, take } from "rxjs/operators";
+import { map, catchError, retryWhen, delay, take, switchMap } from "rxjs/operators";
 import { empty } from "rxjs/observable/empty";
 
 export default function retryLayouts(n = 3, t = 100, silentError = true) {
@@ -8,16 +8,17 @@ export default function retryLayouts(n = 3, t = 100, silentError = true) {
 
         return source$.pipe(
             invalidated(),
-            windowCount(1),
-            map(obs$ => obs$.pipe(
-                getLayout(),
-                retryWhen(err => err.pipe(
-                    delay(t),
-                    take(n)
-                )),
-                catchError(err => silentError ? empty() : err)
-            )),
-            switchAll()
+            switchMap(handle => {
+                var layout$ = handle.call(getLayout());
+
+                return layout$.pipe(
+                    retryWhen(err => err.pipe(
+                        delay(t),
+                        take(n)
+                    )),
+                    catchError(err => silentError ? empty() : err)
+                )
+            })
         );
         
     }
